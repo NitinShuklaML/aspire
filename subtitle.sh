f97e8bd6dfcd0b0d18c96111d654f3c7740e5070
NitinShuklaML <47823372+NitinShuklaML@users.noreply.github.com>
2019-11-14 13:13:19 +0100

    Subtitle.sh at s5 level
    setup.sh at s5 level

--- subtitle.sh
+++ subtitle.sh
@@ -2,8 +2,11 @@
 
 args=$1
 
-../brightcove/authentication.sh $args
-../GenerateTranscript.sh $args
+brightcove/authentication.sh $args
+./GenerateTranscript.sh $args.wav
+./AlignTranscript.sh $args
+
+cat api/map.json
 
 
 
