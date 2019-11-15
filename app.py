948f75c0bb24e1b523c90f7483728d34a2c2c9ce
NitinShuklaML <47823372+NitinShuklaML@users.noreply.github.com>
2019-11-15 12:52:30 +0100

    under s5 folder
    under s5 folder, app.py is flask starter

--- app.py
+++ app.py
@@ -2,7 +2,15 @@
 
 from flask import Flask
 from flask import jsonify
+from subprocess import Popen, PIPE
+from subprocess import check_output
 import run_setup
+
+
+
+def get_subtitle_shell_script(args):
+    stdout = check_output(['./subtitle.sh',str(args)]).decode('utf-8')
+    return stdout
 
 app = Flask(__name__) #app is the name of the object here
 
@@ -13,7 +21,8 @@ def home():
 @app.route('/VideoDownload/<string:videoId>')
 def Video(videoId):
     path="/home/nitin/audio"+videoId+".wav"
-    return jsonify(run_setup.setup(videoId))
+    #return "Under Cnstruction Bro "+videoId
+    return jsonify(get_subtitle_shell_script(videoId))
     #return "<h1>Under Construction Bro" +videoId + "</h1>"
 
 
---
+++ json2vtt.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+
+# Importing json Module for reading the incoming JSON files
+
+import json
+
+# Reading JSON file from current Directory
+
+with open('map.json') as json_file:
+    data = json.load(json_file)
+    
+transcript = json.dumps(data, indent=4)
+
+
+
+# Initializing Empty List
+# Initializimg an empty string for Transcripts
+
+empty=['']
+output="WEBVTT\n\n"
+
+f=open("Sample.vtt","w+")
+
+def convert_time(time,pos):    
+    time = float(frags[pos])    
+    time = time % (24 * 3600)
+    hour = time // 3600    
+    time %= 3600
+    minutes = time // 60
+    time %= 60
+    seconds = time
+    time %= 10
+    millisec = int(time*1000)    
+    if pos=="begin":
+        return ("%02d:%02d:%02d.%03d --> " % (hour, minutes, seconds, millisec))          
+    else:
+        return (" %02d:%02d:%02d.%03d align:middle line:90%%" % (hour, minutes, seconds, millisec) + "\n")          
+
+
+#f=open("Sample.vtt","a+")
+#f.write("WEBVTT\n\n")
+
+#f.write("WEBVTT\n\n")
+
+for frags in data["fragments"]:      
+    if float(frags["end"])-float(frags["begin"]) > 0 and frags["lines"] != empty:
+        #print(float(frags["end"])-float(frags["begin"]))    
+        #line=str(frags["lines"])[2:][:-2]+ "\n"
+        #print(output)
+        output = output + convert_time(float(frags["begin"]),"begin")
+        output = output + convert_time(float(frags["end"]),"end") 
+        output = output + str(frags["lines"])[2:][:-2] + "\n"
+        #output=output + str(frags["lines"])[2:][:-2] + "\n"
+        #f.write(frags["lines"][2:][:-2])
+        #f.write("\n")
+print(output)
+
+f.write(output)
+f.close()
